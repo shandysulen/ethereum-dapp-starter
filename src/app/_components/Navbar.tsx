1878fb8c3e992a0cdce952bc81ba2265ac728502
Shandy Sulen <shandysulen@gmail.com>
2023-11-03 18:50:04 -0400

    remove props from Navbar

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -1,22 +1,9 @@
 import { memo } from "react";
 import { DesktopNavbar } from "./Navbar/DesktopNavbar";
 
-export interface NavbarProps {
-  readonly isDarkMode?: boolean;
-  readonly isScrolled?: boolean;
-}
-
-export const Navbar: React.FC<NavbarProps> = memo(function Navbar({
-  isDarkMode = false,
-  isScrolled = false,
-}) {
-  // const breakpoint = useBreakpoint({ fallback: undefined });
-
+export const Navbar: React.FC = memo(function Navbar() {
   return (
     <header className='z-navbar sticky top-0 w-full border-b bg-background/70 backdrop-blur-3xl'>
-      {/* {["base", "sm"].includes(breakpoint) ? (
-        <MobileNavbar />
-      )} */}
       <DesktopNavbar />
     </header>
   );
