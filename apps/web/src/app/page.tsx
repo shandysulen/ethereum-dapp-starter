dd14109728704b8494765ec8099f4d33bd2dd9c6
Shandy Sulen <shandysulen@gmail.com>
2024-02-25 22:46:53 -0500

    update home page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,5 @@
-import { Container, Heading, HStack } from "@eds/components";
 import "server-only";
+import { Container, Heading, HStack } from "@eds/components";
 import { ConnectButton } from "./_components/ConnectButton";
 import { Description } from "./_components/Description";
 import { LearnMoreButton } from "./_components/LearnMoreButton";
@@ -8,9 +8,9 @@ export default function Index() {
   return (
     <Container
       size='xl'
-      className='flex flex-col justify-center items-center min-h-[calc(100vh-var(--height-header)-var(--height-footer))] h-full gap-4 bg-red-300'
+      className='flex flex-col justify-center items-center min-h-[calc(100vh-var(--height-header)-var(--height-footer))] h-full gap-4'
     >
-      <Heading className='color-purple'>Build the future.</Heading>
+      <Heading>Build the future.</Heading>
       <Description />
       <HStack role='group' className='mt-4'>
         <ConnectButton />
